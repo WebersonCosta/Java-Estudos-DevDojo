d02247c4f1f19e3305081ca53adc3554982fda7a
Weberson Costa <weberson.dev0@gmail.com>
2023-04-09 19:51:04 -0300

    iniciado estudos de execeções em java

---
+++ devDojo/src/academy/devdojo/maratona/javacore/Oexception/runtime/test/RuntimeExceptionTest01.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratona.javacore.Oexception.runtime.test;
+
+public class RuntimeExceptionTest01 {
+
+	public static void main(String[] args) {
+		// Checked e Unchecked
+		
+		/*
+		Object object = null;
+		System.out.println(object.toString());
+		*/
+		
+		int[] num = {1, 2};
+		System.out.println(num[2]);
+	}
+
+}
